3fb7a95060033bbc1d275ede994092a23b319b24
A1doranM <vaselisk1997@gmail.com>
2020-03-31 13:46:40 +0300

    Initial commit

---
+++ src/main/java/Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+    public static void main(String[] args) {
+        Class2 class2 = new Class2();
+        Class3 class3 = new Class3();
+        Class4 class4 = new Class4();
+
+        System.out.println(class2.functionTest() + " " + class3.functionTest() + " " + class4.functionTest());
+    }
+}
---
+++ src/main/java/Class2.java
@@ -0,0 +1,7 @@
+public class Class2 {
+    int functionTest(){
+        int a = 1;
+        System.out.println(a);
+        return a;
+    }
+}
