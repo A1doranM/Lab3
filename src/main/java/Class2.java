54de9561f985c30276045d9be975aa736e703e5e
A1doranM <vaselisk1997@gmail.com>
2020-03-31 13:48:48 +0300

    test

--- src/main/java/Class2.java
+++ src/main/java/Class2.java
@@ -4,4 +4,14 @@ public class Class2 {
         System.out.println(a);
         return a;
     }
+    int functionTest2(){
+        int a = 1;
+        System.out.println(a);
+        return a;
+    }
+    int functionTest3(){
+        int a = 1;
+        System.out.println(a);
+        return a;
+    }
 }
